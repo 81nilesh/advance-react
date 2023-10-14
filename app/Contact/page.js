7a7cdbc0d85c13c299e0dd0cdf5b7f94a412fa80
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-14 17:34:25 +0530

    routing

---
+++ app/Contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <>
+            <h1 className='text-2xl font-bold'>This is Contact page</h1>
+           
+        </>
+    )
+}
+
+export default contact
--- app/page.js
+++ app/page.js
@@ -1,22 +1,11 @@
-'use client';
-import React, { useState } from 'react'
+import React from 'react'
 
 const page = () => {
-  const [username, setUsername] = useState("")
   return (
-    <>
-      <h1 className='mb-5 text-2xl'>Enter your name : </h1>
-      <form>
-        <input type="text" className='border-2
-         border-zinc-800 px-4 py-2 text-xl'
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value)
-            console.log(username)
-          }}
-        />
-      </form>
-    </>
+    <div>
+      <h1 className='text-2xl font-bold'>This is Home page</h1>
+      <a href="/Contact">Contact</a>
+    </div>
   )
 }
 
