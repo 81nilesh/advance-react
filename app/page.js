5bb25da2bdcb96861fa04415f5442daf5fd94a42
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-15 07:51:19 +0530

    dynamic routing

--- app/page.js
+++ app/page.js
@@ -1,12 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import axios from 'axios'
+import Link from 'next/link'
 
 const page = () => {
+  const [users, setUsers] = useState([])
+  const getUsers = async () => {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+    setUsers(data)
+  }
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>This is Home page</h1>
-      <a href="/Contact">Contact</a>
-    </div>
+    <>
+      <button onClick={getUsers} className='bg-green-400 text-white 
+      px-4 py-2
+       rounded font-bold'>Get Data</button>
+      <div className='p-8 bg-slate-100 mt-5'>
+        {users.map((e) => {
+          return <li>{e.username} --- <a href="">Explore</a></li>
+        })}
+      </div>
+    </>
   )
 }
 
-export default page
+export default page;
